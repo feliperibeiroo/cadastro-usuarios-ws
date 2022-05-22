5a23797ae7ed3abb55bac8651479f6096e8f370c
Felipe Ribeiro <felipebezerra35@gmail.com>
2022-05-22 15:47:09 -0300

    add ! a configuracao do knex

--- repositories/usuarioRepository.ts
+++ repositories/usuarioRepository.ts
@@ -3,11 +3,11 @@ import Knex from 'knex'
 const config = process.env.NODE_ENV==='production' ? {
   client: 'pg',
   connection: {
-    host : process.env.POSTGRES_HOST,
-    port : process.env.POSTGRES_PORT,
-    user : process.env.POSTGRES_USER,
-    password : process.env.POSTGRES_PASSWORD,
-    database : process.env.POSTGRES_DATABASE
+    host : process.env.POSTGRES_HOST!,
+    port : process.env.POSTGRES_PORT!,
+    user : process.env.POSTGRES_USER!,
+    password : process.env.POSTGRES_PASSWORD!,
+    database : process.env.POSTGRES_DATABASE!
 }
 } : {
   client: 'pg',
